247ee3f814005c816ccdd243a797a4304bc2e02c
KazuProg <91452566+KazuProg@users.noreply.github.com>
2025-06-11 23:41:56 +0900

    add: trigger url change

--- src/content.js
+++ src/content.js
@@ -1,4 +1,16 @@
 (function () {
+  const isChangedURL = (() => {
+    let previousURL = location.href;
+    return () => {
+      const currentURL = location.href;
+      if (previousURL !== currentURL) {
+        previousURL = currentURL;
+        return true;
+      }
+      return false;
+    };
+  })();
+
   const handleRecognitionSuccess = (title, artist) => {
     console.log(`曲名: ${title}, アーティスト: ${artist}`);
     triggerServerCommunication({
@@ -6,6 +18,24 @@
       title: title,
       artist: artist,
     });
+  };
+
+  const handleURLChange = () => {
+    const currentURL = location.href;
+    console.log(`URLが変更されました: ${currentURL}`);
+
+    if (location.pathname.indexOf("/song/") !== -1) {
+      const title =
+        document.querySelector("main h1")?.textContent?.trim() || "";
+      const artist =
+        document.querySelector("main h2")?.textContent?.trim() || "";
+      console.log(`曲名: ${title}, アーティスト: ${artist}`);
+      triggerServerCommunication({
+        type: "SHAZAM_SONG_PAGE_VIEW",
+        title: title,
+        artist: artist,
+      });
+    }
   };
 
   window.addEventListener("DOMContentLoaded", () => {
@@ -22,8 +52,32 @@
 
           handleRecognitionSuccess(title, artist);
         }
+        if (mutation.removedNodes.length !== 0) {
+          const node = mutation.removedNodes[0];
+          if (node.className.indexOf("MatchInterstitial") == -1) continue;
+
+          // TODO: もう少しURL検知を高速化
+          if (isChangedURL()) handleURLChange();
+        }
       }
     }).observe(document.body.querySelectorAll(":scope > div")[1], {
+      childList: true,
+      subtree: false,
+    });
+
+    // ページ遷移を監視するためのMutationObserverを設定
+    new MutationObserver((mutationsList, observer) => {
+      for (const mutation of mutationsList) {
+        if (mutation.type !== "childList") continue;
+        if (mutation.removedNodes.length !== 0) {
+          const node = mutation.removedNodes[0];
+          if (node?.classList?.toString().indexOf("Transition_bar") == -1)
+            continue;
+
+          if (isChangedURL()) handleURLChange();
+        }
+      }
+    }).observe(document.body, {
       childList: true,
       subtree: false,
     });
